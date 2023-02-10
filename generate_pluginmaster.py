94479287602bee33dd2daf7c28bf201aedcd2f32
Athavar <9285323+Athavar@users.noreply.github.com>
2023-02-10 06:26:32 +0100

    Update generate_pluginmaster.py

--- generate_pluginmaster.py
+++ generate_pluginmaster.py
@@ -1,10 +1,7 @@
 import json
 import os
-from time import time
-from sys import argv
-from zipfile import ZipFile, ZIP_DEFLATED
+from zipfile import ZipFile
 import subprocess
-
 
 BRANCH = os.environ['GITHUB_REF'].split('refs/heads/')[-1]
 DOWNLOAD_URL = 'https://github.com/Athavar/Athavar.FFXIV.DalaRepo/raw/{branch}/plugins/{plugin_name}/latest.zip'
@@ -41,6 +38,7 @@ TRIMMED_KEYS = [
     'DownloadLinkInstall',
 ]
 
+
 def main():
     # extract the manifests from inside the zip files
     master = extract_manifests()
@@ -57,27 +55,40 @@ def main():
     # update the LastUpdated field in master
     last_updated()
 
+
 def extract_manifests():
     manifests = []
 
     for dirpath, dirnames, filenames in os.walk('./plugins'):
-        if len(filenames) == 0 or 'latest.zip' not in filenames:
+        if len(filenames) == 0:
             continue
+
         plugin_name = dirpath.split('/')[-1]
-        latest_zip = f'{dirpath}/latest.zip'
-        with ZipFile(latest_zip) as z:
-            manifest = json.loads(z.read(f'{plugin_name}.json').decode('utf-8'))
-            manifests.append(manifest)
+        if plugin_name + '.json' in filenames:
+            latest_manifest = f'{dirpath}/{plugin_name}.json'
+            with open(latest_manifest) as f:
+                manifest = json.loads(f.read())
+                print(manifest)
+                manifests.append(manifest)
+            continue
+
+        if 'latest.zip' in filenames:
+            latest_zip = f'{dirpath}/latest.zip'
+            with ZipFile(latest_zip) as z:
+                manifest = json.loads(z.read(f'{plugin_name}.json').decode('utf-8'))
+                manifests.append(manifest)
+            continue
 
     return manifests
+
 
 def add_extra_fields(manifests):
     for manifest in manifests:
         # generate the download link from the internal assembly name
-        
-        if not manifest.get('DownloadLinkInstall'):        
+
+        if not manifest.get('DownloadLinkInstall'):
             manifest['DownloadLinkInstall'] = DOWNLOAD_URL.format(branch=BRANCH, plugin_name=manifest["InternalName"])
-            
+
         # add default values if missing
         for k, v in DEFAULTS.items():
             if k not in manifest:
@@ -89,21 +100,25 @@ def add_extra_fields(manifests):
                     manifest[k] = manifest[source]
         manifest['DownloadCount'] = 0
 
+
 def write_master(master):
     # write as pretty json
     with open('pluginmaster.json', 'w') as f:
         json.dump(master, f, indent=4)
 
+
 def trim_manifest(plugin):
     return {k: plugin[k] for k in TRIMMED_KEYS if k in plugin}
 
+
 def git(*args):
     return subprocess.check_output(['git'] + list(args), universal_newlines=True)
-    
+
+
 def last_updated():
     with open('pluginmaster.json') as f:
         master = json.load(f)
-        
+
     for plugin in master:
         latest = f'plugins/{plugin["InternalName"]}/latest.zip'
         modified = int(git('log', '-1', '--format=%at', latest).strip('\n'))
@@ -113,6 +128,7 @@ def last_updated():
 
     with open('pluginmaster.json', 'w') as f:
         json.dump(master, f, indent=4)
+
 
 if __name__ == '__main__':
     main()
